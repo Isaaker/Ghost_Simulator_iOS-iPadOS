be255265a239b92d90b2a401c7a5c30c24b22087
Isaaker <77550577+Isaaker@users.noreply.github.com>
2023-01-28 08:31:56 +0100

    Create app

---
+++ ghost_archery_sim/ghost_archery_sim/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ghost_archery_sim
+//
+//  Created by Isaac Hernán Martí on 28/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ghost_archery_sim/ghost_archery_sim/ghost_archery_simApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghost_archery_simApp.swift
+//  ghost_archery_sim
+//
+//  Created by Isaac Hernán Martí on 28/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ghost_archery_simApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
